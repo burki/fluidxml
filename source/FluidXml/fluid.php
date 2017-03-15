4d682b026911250be89e0f48b80c125b9ed1bd1a
burki <daniel.burckhardt@sur-gmbh.ch>
2017-03-15 08:35:29 +0100

    Update fluid.php
    Export short functions only if variadic arguments are supported

--- source/FluidXml/fluid.php
+++ source/FluidXml/fluid.php
@@ -4,17 +4,21 @@ namespace FluidXml;
 
 define('FLUIDXML_VERSION', '1.20.2');
 
-function fluidxml(...$arguments)
-{
-        return new \FluidXml\FluidXml(...$arguments);
-}
+if (version_compare(phpversion(), '5.6.0', '>=')) {
 
-function fluidify(...$arguments)
-{
-        return \FluidXml\FluidXml::load(...$arguments);
-}
+        function fluidxml(...$arguments)
+        {
+                return new \FluidXml\FluidXml(...$arguments);
+        }
 
-function fluidns(...$arguments)
-{
-        return new \FluidXml\FluidNamespace(...$arguments);
+        function fluidify(...$arguments)
+        {
+                return \FluidXml\FluidXml::load(...$arguments);
+        }
+
+        function fluidns(...$arguments)
+        {
+                return new \FluidXml\FluidNamespace(...$arguments);
+        }
+
 }
