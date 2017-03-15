7889e4d2f25fc61ced3ec9ebd6eb1dd701854a07
burki <daniel.burckhardt@sur-gmbh.ch>
2017-03-15 08:54:42 +0100

    Update fluid.php

--- source/FluidXml/fluid.php
+++ source/FluidXml/fluid.php
@@ -4,28 +4,26 @@ namespace FluidXml;
 
 define('FLUIDXML_VERSION', '1.20.2');
 
-if (version_compare(phpversion(), '5.6.0', '>=')) {
-        /* 
-          this still leads to syntax errors in older versions 
-          
-          TODO: adjust to something like
-            $reflection = new ReflectionClass( 'yourClassName' );
-            return $reflection->newInstanceArgs( func_get_args() );
-        */
-
-        function fluidxml(...$arguments)
-        {
-                return new \FluidXml\FluidXml(...$arguments);
-        }
-
-        function fluidify(...$arguments)
-        {
-                return \FluidXml\FluidXml::load(...$arguments);
-        }
-
-        function fluidns(...$arguments)
-        {
-                return new \FluidXml\FluidNamespace(...$arguments);
-        }
-
+/* 
+ * the following leads to syntax errors in PHP < 5.6 
+ *
+ *  TODO: adjust to something like
+ *   $reflection = new ReflectionClass( 'yourClassName' );
+ *   return $reflection->newInstanceArgs( func_get_args() );
+ 
+function fluidxml(...$arguments)
+{
+        return new \FluidXml\FluidXml(...$arguments);
 }
+
+function fluidify(...$arguments)
+{
+        return \FluidXml\FluidXml::load(...$arguments);
+}
+
+function fluidns(...$arguments)
+{
+        return new \FluidXml\FluidNamespace(...$arguments);
+}
+
+*/
