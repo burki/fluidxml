5ea43022bec821282a81c339973cc679c6f67f9f
burki <daniel.burckhardt@sur-gmbh.ch>
2017-03-15 08:52:01 +0100

    Update fluid.php

--- source/FluidXml/fluid.php
+++ source/FluidXml/fluid.php
@@ -5,6 +5,13 @@ namespace FluidXml;
 define('FLUIDXML_VERSION', '1.20.2');
 
 if (version_compare(phpversion(), '5.6.0', '>=')) {
+        /* 
+          this still leads to syntax errors in older versions 
+          
+          TODO: adjust to something like
+            $reflection = new ReflectionClass( 'yourClassName' );
+            return $reflection->newInstanceArgs( func_get_args() );
+        */
 
         function fluidxml(...$arguments)
         {
